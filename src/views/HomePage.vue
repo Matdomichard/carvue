46fddfc2e2ab8c2050873d6e0c2de7481f6eccb4
matdomichard <mathieu.domichard@gmail.com>
2023-02-13 12:34:04 +0100

    Ajout de la suppression de dates

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -13,9 +13,12 @@
     <h1>Dates enregistrées</h1>
     <template v-if="userStore.$state.user.id">
       <ul>
-        <li v-for="date in savedDates" :key="date.name" class="flex">
-          <div class="w-1/2">{{ date.name }}
+        <li v-for="(date, index) in savedDates" :key="date.name" class="flex">
+          <div>{{ date.name }}
             <DateCalculator :date="date.date" />
+          </div>
+          <div>
+            <i @click="deleteSavedDate(date.id, index)">❌</i>
           </div>
         </li>
       </ul>
@@ -31,8 +34,6 @@
     </template>
   </div>
 </template>
-
-
 <script lang="ts">
 import { ref, onMounted, onUnmounted, watch } from 'vue';
 import axios from 'axios';
@@ -49,7 +50,7 @@ export default {
       name: '',
       selectedDate: '',
     });
-    const savedDates = ref<{ name: string, date: string }[]>([]);
+    const savedDates = ref<{ name: string, date: string, id: number }[]>([]);
 
     const saveDate = () => {
       if (userStore.$state.user.id) {
@@ -60,10 +61,11 @@ export default {
           name: form.value.name,
           date: form.value.selectedDate,
         }, { headers })
-          .then(() => {
+          .then((response) => {
             savedDates.value.push({
               name: form.value.name,
               date: form.value.selectedDate,
+              id: response.data.id,
             });
             form.value.name = '';
             form.value.selectedDate = '';
@@ -76,7 +78,25 @@ export default {
       }
     };
 
+    const deleteSavedDate = (dateId: number, index: number) => {
+      if (userStore.$state.user.id) {
+        const headers = {
+          Authorization: `Bearer ${userStore.$state.user.token}`,
+        };
+        axios.delete(`dates/${userStore.$state.user.id}/${dateId}`, { headers })
+          .then(() => {
+            savedDates.value.splice(index, 1);
+          })
+          .catch((error) => {
+            console.error(error);
+          });
+      } else {
+        console.error("L'ID de l'utilisateur est manquant.");
+      }
+    };
+
     const fetchSavedDates = () => {
+      console.log('userId',userStore.$state.user.id);
       if (userStore.$state.user.id) {
         const headers = {
           Authorization: `Bearer ${userStore.$state.user.token}`,
@@ -102,6 +122,7 @@ export default {
       userStore,
       savedDates,
       saveDate,
+      deleteSavedDate,
     };
   },
 };
