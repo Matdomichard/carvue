bfd27f582107009d1afa2eb05dad8456f536fbd5
matdomichard <mathieu.domichard@gmail.com>
2023-05-09 14:08:24 +0200

    Ajout de dotenv

--- src/plugins/axios.ts
+++ src/plugins/axios.ts
@@ -1,5 +1,8 @@
 import axios, { AxiosInstance } from 'axios';
 
+import dotenv from 'dotenv';
+dotenv.config();
+
 const instance: AxiosInstance = axios.create({
   baseURL: process.env.VITE_API_URL,
   headers: {
