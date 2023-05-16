9ca3997b54c84c400d092bc6938d1bde3d2d33fd
matdomichard <mathieu.domichard@gmail.com>
2023-05-16 23:20:31 +0200

    Modification de baseurl

--- src/plugins/axios.ts
+++ src/plugins/axios.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from 'axios';
 
 const instance: AxiosInstance = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://comptearebours.herokuapp.com',
   headers: {
     'Content-Type': 'application/json',
   },
